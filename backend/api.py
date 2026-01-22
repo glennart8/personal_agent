d31667161581355cebc187f9094e88962038764a
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2026-01-22 16:51:07 +0100

    moved some functions into api and frontend to record in browser with streamlit and easier serve the transcribed text through api and save it to db and csv file

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from rag_agent import diary_agent
+from stt_agents import stt_agent
 from data_models import Prompt
+from datetime import datetime
+import locale
+from data_ingestion import add_data
 
+# Måste ha detta för att få ut svenska ord för veckodagarna..........
+try:
+    locale.setlocale(locale.LC_TIME, "sv_SE.UTF-8")
+except:
+    pass # Blir engelska ord i stället
 
 app = FastAPI()
 
@@ -14,3 +23,19 @@ async def search_vector_db(query: Prompt):
     result = await diary_agent.run(query.prompt)
     
     return result.output
+
+@app.post("/add_text")
+async def add_text(query: Prompt):
+    result = await stt_agent.run(f"Analysera detta dagboksinlägg: {query.prompt}")
+    
+    new_entry = {
+        "date": datetime.now().strftime("%Y-%m-%d"),
+        "weekday": datetime.now().strftime("%A").capitalize(),
+        "activity": result.output.activity,
+        "mood": result.output.mood
+    }
+    print(new_entry)
+    add_data(new_entry)
+    
+    return new_entry
+    
--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,46 @@
 import os
 import streamlit as st
 import requests
+from speech_to_text import get_devices, record_audio, transcribe_audio
+from pathlib import Path
 
 BACKEND_BASE_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
+# AUDIO_PATH = Path(__file__).parents[0] / "recording.wav"
 
+# input = st.text_input(label = "Vad undrar du?")
 
-input = st.text_input(label = "Vad undrar du?")
-
-if input:
-    answer=requests.post(f"{BACKEND_BASE_URL}/query", json={"prompt": input})
+# if input:
+#     answer=requests.post(f"{BACKEND_BASE_URL}/query", json={"prompt": input})
     
-    st.write(answer.json())
+#     st.write(answer.json())
+    
+    
+def layout():
+    # input_info = get_devices()
+    # output_info = get_devices("output")
+    # # st.write(output_info)
+    
+    # name = input_info["name"]
+    # output_name = output_info["name"]
+    
+    # id = int(input_info["index"])
+    # samplerate = int(input_info["default_samplerate"])
+    # channels = int(input_info["max_input_channels"])
+    
+    # st.write(f"Input device: {name}")
+    # st.write(f"Output device: {output_name}")
+    
+    # if st.button("Record Audio"):
+    #     transcription = record_audio(AUDIO_PATH, samplerate, id, channels)
+        
+    #     st.write(transcription)
+    
+    audio = st.audio_input("Record")
+    if audio:
+        text = transcribe_audio(audio)
+        st.write(text)
+        requests.post(f"{BACKEND_BASE_URL}/add_text", json={"prompt": text})
+
+
+if __name__ == "__main__":
+    layout()
