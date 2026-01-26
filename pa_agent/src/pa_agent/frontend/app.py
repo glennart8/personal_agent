0b6ceee4b42988bbf1726529a3b7fcfba4135d57
glennart8 <henrikpilback@gmail.com>
2026-01-26 15:17:49 +0100

    Added a functions for some KPI's in streamlit, more to come

--- pa_agent/src/pa_agent/frontend/app.py
+++ pa_agent/src/pa_agent/frontend/app.py
@@ -8,23 +8,35 @@ BACKEND_BASE_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
 
 get_css()
 
-def show_activities(mood: str, count = 5):
+
+def show_activities(df, mood: str, count = 5):
     """
     Use either 'positivt' or 'negativt' as in parameter
     Returns a list of activities which could be linked to negative feelings 
     """
-    response = requests.get(f"{BACKEND_BASE_URL}/diary")
-    df = pd.DataFrame(response.json())
     df = df[df['mood'] == mood]
     
     # Få de mest förekommande aktiviteterna
     activities = df['activity'].value_counts().head(count).index.tolist()
     
     # Formatera som text
-    return "\n".join([f"{activity}\n" for activity in activities])
+    return "\n".join([f"{i+1}. {activity}\n" for i, activity in enumerate(activities)])
     
     
+def show_kpis(df, mood):
+    all = len(df)
+    df_mood = df[df['mood'] == mood] 
+    percentage = f"{round(len(df_mood) / all * 100)}%"
+    
+    # MIN SÄMSTA DAG
+    worst_or_best_day = df.query("mood == 'negativt'")['weekday'].value_counts().idxmax()
+    return percentage, worst_or_best_day
+     
+     
 def layout():
+    response = requests.get(f"{BACKEND_BASE_URL}/diary")
+    df = pd.DataFrame(response.json())
+    
     tab1, tab2, tab3 = st.tabs(["Dashboard", "Diary", "News"])
     
     with tab1:
@@ -37,9 +49,12 @@ def layout():
                 st.header("DARK SIDE")
                 st.subheader("This makes you feel shitty..")
                 mood = "negativt"
-                activities = show_activities(mood)
+                activities = show_activities(df, mood)
                 st.write(activities)
                 
+                # --- KPIS ---
+                st.write(show_kpis(df, mood))
+                                
                 st.markdown('</div>', unsafe_allow_html=True)
 
         with col2:
@@ -71,8 +86,10 @@ def layout():
             st.header("HAPPY PLACE")
             st.subheader("Sunshine, melted ice cream and drink sticks")
             mood = "positivt"
-            activities = show_activities(mood)
+            activities = show_activities(df, mood)
             st.write(activities)
+            
+            st.write(show_kpis(df, mood))
             
             st.markdown('</div>', unsafe_allow_html=True)
 
@@ -81,14 +98,11 @@ def layout():
         
         with col1:
             st.header("Diary")
-            response = requests.get(f"{BACKEND_BASE_URL}/diary")
-            df = pd.DataFrame(response.json())
-            df = df.sort_values(by="date", ascending=False)
-            st.dataframe(df, use_container_width=True)
+            diary_df = df.sort_values(by="date", ascending=False)
+            st.dataframe(diary_df, use_container_width=True)
         
         with col2:
             pass
-
 
     with tab3:
         pass
