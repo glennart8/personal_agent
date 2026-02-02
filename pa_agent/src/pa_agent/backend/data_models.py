586f83f724d99e03b30b92cd1e877389a41a1e51
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2026-02-02 20:27:43 +0100

    changed to json file with keywords in setup_db.py, added functionality to make all keywords and moods with capitalized first letter, added mood and keywords to lancedb news schema data model.

--- pa_agent/src/pa_agent/backend/data_models.py
+++ pa_agent/src/pa_agent/backend/data_models.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from lancedb.embeddings import get_registry
 from lancedb.pydantic import LanceModel, Vector
 from dotenv import load_dotenv
-from pathlib import Path
+# from pathlib import Path
 from typing import Optional, Literal
 
 load_dotenv()
 
-#embedding_model = get_registry().get("gemini-text").create(name="gemini-embedding-001")
-embedding_model = get_registry().get("openai").create(name="text-embedding-3-large")
+embedding_model = get_registry().get("gemini-text").create(name="gemini-embedding-001")
+# embedding_model = get_registry().get("openai").create(name="text-embedding-3-large")
 
 
 #region DIARY
@@ -31,12 +31,12 @@ class DiaryExtraction(BaseModel):
     mood: str = Field(description="Positive or negative")
     keywords: str = Field(description="Extract 1-3 tags/keywords that categorize the entry, e.g. ['Jobb', 'Sömn', 'Kodning', 'Relationer', 'Träning']")
 
-# class SingleArticleAnalysis(BaseModel):
-#     mood: str = Field(description="Positive or Negative")
-#     keywords: str = Field(description="Extract 1-3 tags/keywords that categorize the entry, e.g. 'finance, politics, sport'")
+class SingleArticleAnalysis(BaseModel):
+    mood: str = Field(description="Positive or Negative")
+    keywords: str = Field(description="Extract 1-3 tags/keywords that categorize the entry, e.g. 'finance, politics, sport'")
 
-# class NewsExtraction(BaseModel):
-#     articles: list[SingleArticleAnalysis]
+class NewsExtraction(BaseModel):
+    articles: list[SingleArticleAnalysis]
     
     
 #region NEWS
@@ -56,6 +56,8 @@ class News(LanceModel):
     teaser_text: str = embedding_model.SourceField()
     image_url: Optional[str] = None
     image_description: Optional[str] = None
+    mood: str
+    keywords: str
     embedding: Vector(dim=3072) = embedding_model.VectorField()
 
 
@@ -77,9 +79,12 @@ class RoutingDescision(BaseModel):
     intent: str = Literal["ENTRY", "QUERY"]
 
 
-# En klass så vi kan använda olika röster
-# Använder Path för att kunna köra stem() och read_text()
-class TTSConfig(BaseModel):
-    input_file: Path = Field(..., description="Sökväg till textfilen som ska läsas")
-    output_file: Optional[Path] = None
-    voice: str = Field("sv-SE-SofieNeural") # "sv-SE-MattiasNeural"
+# # En klass så vi kan använda olika röster
+# # Använder Path för att kunna köra stem() och read_text()
+# class TTSConfig(BaseModel):
+#     input_file: Path = Field(..., description="Sökväg till textfilen som ska läsas")
+#     output_file: Optional[Path] = None
+#     voice: str = Field("sv-SE-SofieNeural") # "sv-SE-MattiasNeural"
+
+# En modell för news som LLM plockar ut
+
--- pa_agent/src/pa_agent/setup_db.py
+++ pa_agent/src/pa_agent/setup_db.py
@@ -3,7 +3,7 @@ import pandas as pd
 from backend.data_models import Daily_mood, Article, News
 from backend.constants import VECTOR_DATABASE_PATH, DATA_PATH
 import time
-import json
+# import json
 
 def setup_vector_db(table: str, path=VECTOR_DATABASE_PATH):
     vector_db = lancedb.connect(uri=path)
@@ -22,10 +22,12 @@ def setup_vector_db(table: str, path=VECTOR_DATABASE_PATH):
     if table == "news":
         vector_db.create_table(name="news", schema=News, mode="overwrite")
         news_table = vector_db.open_table("news")
-        ingest_crawl_to_vector_db(news_table, DATA_PATH / "omni_cleaned.json")
+        ingest_crawl_to_vector_db(news_table, DATA_PATH / "omni_cleaned_with_keywords.json")
     
 
 def ingest_csv_to_vector_db(table):
+    print("Loading diary data..")
+    
     file_path = DATA_PATH / "dagbok.csv"
     df = pd.read_csv(file_path)
     
@@ -40,6 +42,8 @@ def ingest_csv_to_vector_db(table):
 
     )
     table.merge_insert("content").when_matched_update_all().when_not_matched_insert_all().execute(df)
+    
+    print(f"Inserted {len(df)} rows of diary data to vectorDB")
 
 
 
@@ -72,10 +76,10 @@ def ingest_txt_to_vector_db(table, file_path, chunk_size=1000):
             print(f"Sparade batch {i} till {i + len(batch)} av {len(data_to_ingest)}")
         except Exception as e:
             print(f"Fel vid batch {i}: {e}")
-            time.sleep(5) # Vänta längre om det blir fel
+            time.sleep(40) # Vänta längre om det blir fel
             
         # Kort paus
-        time.sleep(1)
+        time.sleep(20)
 
 
 def ingest_crawl_to_vector_db(table, file_path): 
@@ -84,8 +88,11 @@ def ingest_crawl_to_vector_db(table, file_path):
     """
     df_cleaned = pd.read_json(file_path)
     batch_size = 50
+    sleep_time = 30
 
     df_cleaned["date"] = pd.to_datetime(df_cleaned["date"]).dt.strftime('%Y-%m-%d')
+    df_cleaned["mood"] = df_cleaned["mood"].str.capitalize()
+    
     print(f"Data will be loaded in batches of batch size: {batch_size}")
     for i in range(0, len(df_cleaned), batch_size):
         print("Starting to load data...")
@@ -93,8 +100,8 @@ def ingest_crawl_to_vector_db(table, file_path):
 
         #merge_insert så man inte lägger in dubletter
         table.merge_insert("title").when_matched_update_all().when_not_matched_insert_all().execute(batch_df)
-        print(f"Finished loading batch {i}-{i+batch_size if i+batch_size < len(df_cleaned) else len(df_cleaned)} of {len(df_cleaned)}\nSleeping for 30 seconds")
-        time.sleep(30) 
+        print(f"Finished loading batch {i}-{i+batch_size if i+batch_size < len(df_cleaned) else len(df_cleaned)} of {len(df_cleaned)}\nSleeping for {sleep_time} seconds")
+        time.sleep(sleep_time) 
         
     print("Load finished")
 
@@ -102,22 +109,22 @@ if __name__ == "__main__":
     print("Sätter upp LanceDB...")
     
     # Kommentera ut table som du inte vill overwrita, tex 'science'
-    setup_vector_db("diary")
+    # setup_vector_db("diary")
     # setup_vector_db("science")
     # setup_vector_db("news")
     
     
     # ----- Lägg till ny data utan att overwrita table -----
     
-    # db = lancedb.connect(uri=VECTOR_DATABASE_PATH)
+    db = lancedb.connect(uri=VECTOR_DATABASE_PATH)
     
     # diary_table = db.open_table("diary")
     # science_table = db.open_table("science")
-    # news_table = db.open_table("news")
+    news_table = db.open_table("news")
     
     # ingest_csv_to_vector_db(diary_table)
     # ingest_txt_to_vector_db(science_table, DATA_PATH / "whr25.txt", chunk_size=1000)
     # ingest_txt_to_vector_db(science_table, DATA_PATH / "the-perma-model.txt", chunk_size=1000)
-    # ingest_crawl_to_vector_db(news_table, DATA_PATH / "omni_cleaned.json")
+    ingest_crawl_to_vector_db(news_table, DATA_PATH / "omni_cleaned_with_keywords.json")
     
     print("Db klar!")
